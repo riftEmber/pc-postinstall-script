fbd7ed67c969064a51342377db87fbb907ea1ffc
Anna Rift <ultimateartificer@gmail.com>
2026-05-24 14:17:04 -0600

    Remove unnecessary comments

--- postinstall.bash
+++ postinstall.bash
@@ -4,13 +4,11 @@
 
 set -exuo pipefail
 
-# Install basic requirements to run the Ansible playbook
 echo ">> Updating all installed packages"
 sudo pacman -Syu --noconfirm
 echo ">> Installing required packages for bootstrapping"
 sudo pacman -S --noconfirm python ansible github-cli firefox git
 
-# Generate new SSH key
 echo ">> Generating SSH key"
 ssh-keygen -t ed25519 -C "$USER@$(cat /etc/hostname)" -f ~/.ssh/id_ed25519 -N ""
 
@@ -20,7 +18,6 @@ gh auth login --hostname github.com --git-protocol ssh --insecure-storage
 echo ">> Logging back out of Github to clear secrets"
 gh auth logout
 
-# Run Ansible playbook
 echo ">> Pulling and running Ansible playbook"
 ansible-pull --url git@github.com:riftEmber/pc-config setup.yaml -i localhost, --ask-become-pass
 
