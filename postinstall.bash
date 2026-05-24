62b5aba734e57930bf8c64a49d34eb62d40c5e72
Anna Rift <ultimateartificer@gmail.com>
2026-05-24 14:17:14 -0600

    Add Github SSH keys to known hosts

--- postinstall.bash
+++ postinstall.bash
@@ -7,12 +7,15 @@ set -exuo pipefail
 echo ">> Updating all installed packages"
 sudo pacman -Syu --noconfirm
 echo ">> Installing required packages for bootstrapping"
-sudo pacman -S --noconfirm python ansible github-cli firefox git
+sudo pacman -S --noconfirm python ansible github-cli firefox git jq
 
 echo ">> Generating SSH key"
 ssh-keygen -t ed25519 -C "$USER@$(cat /etc/hostname)" -f ~/.ssh/id_ed25519 -N ""
 
-# Log in to GitHub
+echo ">> Adding GitHub SSH keys to known hosts"
+curl --silent https://api.github.com/meta \
+  | jq --raw-output '"github.com "+.ssh_keys[]' >> ~/.ssh/known_hosts
+
 echo ">> Logging in to GitHub, make sure to upload SSH key"
 gh auth login --hostname github.com --git-protocol ssh --insecure-storage
 echo ">> Logging back out of Github to clear secrets"
